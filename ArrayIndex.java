61281afb6c9e525129434fb547c64f90c43564d4
-Rudy- <rudyb2001@gmail.com>
2018-07-03 20:15:35 -0700

    Add files via upload

---
+++ ArrayIndex.java
@@ -0,0 +1,14 @@
+
+
+class ArrayIndex {
+	public static void main(String[] args) {
+		
+		System.out.println("Index\tValue");
+		int nums[] = {2, 4, 5, 6, 7, 8, 10};
+		
+		for (int counter = 0; counter < nums.length; counter++) {
+			System.out.println(counter + "\t" + nums[counter]);
+		}
+		
+	}
+}
